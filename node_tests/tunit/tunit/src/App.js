c3a66773ec7f82bec8a539063b81ed0e29465d89
Elijah Larios <elijahlarios@icloud.com>
2023-03-07 22:36:51 -0800

    converting base to js

---
+++ node_tests/tunit/tunit/src/App.js
@@ -0,0 +1,37 @@
+// import logo from './logo.svg';
+import './Navbar.css';
+import Navbar from './Navbar.js';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ node_tests/tunit/tunit/src/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import placeholder from './images/placeholder.jpg'
+import { Stack, HStack } from '@chakra-ui/react'
+
+// import { Stack, HStack, VStack } from '@chakra-ui/react'
+
+function Navbar() {
+    return (
+        <HStack className='container'>
+            <h1>
+                Today's Recommendations
+            </h1>
+            <a href="./profile.js">
+                <img className='circle resize' src={placeholder} />
+            </a>
+        </HStack>
+    )
+    
+}
+
+export default Navbar;
