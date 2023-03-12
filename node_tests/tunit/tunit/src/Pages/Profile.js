3326d2de3f39f8ae749a580495a9340b1db3d91f
Elijah Larios <elijahlarios@icloud.com>
2023-03-11 19:32:42 -0800

    getting json stringify ready for db

--- node_tests/tunit/tunit/src/Pages/Profile.js
+++ node_tests/tunit/tunit/src/Pages/Profile.js
@@ -4,6 +4,7 @@ import '../css/Navbar.css'
 import '../css/fuzz.css'
 import '../css/Profile.css'
 import { NavLink } from 'react-router-dom';
+import useFetch from "react-fetch-hook";
 
 
 
@@ -13,8 +14,22 @@ const lastName ="Doe"
 const emailAddr ="johndoe@email.com"
 const authFormat = "Apple Music"
 function Profile() {
+
+    const { isLoading, error, rawData } = useFetch(
+        "https://jsonplaceholder.typicode.com/users"
+        );
+        if (isLoading) return "Loading...";
+        if (error) return "Error";
+    
+    
+       const rawDataStringify = JSON.stringify(rawData, null, 2);
+       let name = rawDataStringify['name'];
+
+    //stopping for the night; pickup at: https://www.w3schools.com/js/js_json_stringify.asp
+
     return (
         <>
+            {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
             <nav>
                 <NavLink to="/">
                     <h2>
@@ -25,7 +40,7 @@ function Profile() {
             <VStack>
                 <p></p>
                 <h3>Name</h3>
-                <p>{firstName} {lastName}</p>
+                <p>{name[0]}</p>
                 <br></br>
                 <h3>Email</h3>
                 <p> {emailAddr}</p>
