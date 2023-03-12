e88f27c49a90e5c948b7516cf2893f69d1b7cd1e
Elijah Larios <elijahlarios@icloud.com>
2023-03-12 11:38:25 -0700

    add a '/'

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='https://tunit-ai.github.io/Tunit-AI'>
+    <BrowserRouter basename='https://tunit-ai.github.io/Tunit-AI/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
